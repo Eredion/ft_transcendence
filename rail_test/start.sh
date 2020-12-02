9c8a3631030c163bb895848a96702174430bf82a
David R <egoz3ntrum@gmail.com>
2020-12-02 10:21:06 +0100

    martes

--- rail_test/start.sh
+++ rail_test/start.sh
@@ -3,7 +3,7 @@
 rails new myapp
 cd myapp
 bundle update && bundle install
-curl -o- -L https://yarnpkg.com/install.sh  | bash
+
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 yarn install --check-files
 rails webpacker:install

84955d3c0f1cecebf879331d733521604d2d68b1
Eredion <alvaro.segovia@hotmail.com>
2020-12-03 10:05:03 +0100

    Improved dockerfile

--- rail_test/README.md
+++ rail_test/README.md
@@ -1,13 +1,6 @@
-Instrucciones para ejecutarlo.
+Instrucciones para ejecutarlo:
 
-1) Constuir la imagen:
+docker-compose up --build
 
-docker-compose build
-
-La imagen se construye con el nombre indicado en el .yaml
-
-2) Levantar la imagen con el nombre indicado previamente. 
-
-docker-copose run --rm --service-ports rails_service
 
 
--- rail_test/start.sh
+++ rail_test/start.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-rails new myapp
+#Actualizamos los paquetes y las ultimas dependencias
 cd myapp
 bundle update && bundle install
-
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 yarn install --check-files
 rails webpacker:install
+
+#Ejecutamos el servicio en localhost y el puerto indicado
 rails server -p $PORT -b 0.0.0.0
 

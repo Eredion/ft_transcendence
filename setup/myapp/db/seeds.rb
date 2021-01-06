046dfcb07556aa9c4e5b34795fbf70e542b670f4
David R <egoz3ntrum@gmail.com>
2021-01-06 17:16:47 +0100

    online user buttons added to chat tab

--- setup/myapp/db/seeds.rb
+++ setup/myapp/db/seeds.rb
@@ -7,3 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(nickname: 'marvin', email: 'marvin@marvin.com')
+User.create(nickname: 'stouf', email: 'pabgonza@marvin.com')
+User.create(nickname: 'apita-da', email: 'apita@marvin.com')
+User.create(nickname: 'migferna', email: 'migferna@marvin.com')
+User.create(nickname: 'marvina', email: 'marvina@marvin.com')
+User.create(nickname: 'marvino', email: 'marvino@marvin.com')
+User.create(nickname: 'marvinu', email: 'marvinu@marvin.com')
+User.create(nickname: 'marvinx', email: 'marvinx@marvin.com')
+User.create(nickname: 'marvinillo', email: 'marvinillo@marvin.com')
+User.create(nickname: 'marvinete', email: 'marvinete@marvin.com')
+User.create(nickname: 'marvinette', email: 'marvinette@marvin.com')
+User.create(nickname: 'ledesma', email: 'xxxxgmail@marvin.com')
+User.create(nickname: 'santana', email: 'blacked@marvin.com')
+User.create(nickname: 'castela', email: 'castela@marvin.com')
+User.create(nickname: 'luismicrack', email: 'luismicrack@marvin.com')
+

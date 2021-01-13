e4f31c33347a7bbd900ca8f6f72d50cdf6b066d9
David R <egoz3ntrum@gmail.com>
2021-01-13 13:03:16 +0100

    fixed caching errors in development.rb

--- setup/myapp/config/environments/development.rb
+++ setup/myapp/config/environments/development.rb
@@ -19,16 +19,18 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
+    
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
+  
     config.cache_store = :null_store
   end
+
+  config.serve_static_assets = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
@@ -64,6 +66,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts << "rails"
 end

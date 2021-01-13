bc33ae322ed25e9f89d9ccc30a2a55c6ecfa8eb7
David R <egoz3ntrum@gmail.com>
2021-01-13 18:01:25 +0100

    rails online-users-buttons 30% done

--- setup/myapp/app/controllers/chats_controller.rb
+++ setup/myapp/app/controllers/chats_controller.rb
@@ -1,8 +1,13 @@
 class ChatsController < ApplicationController
 	def create
-        @chat = Chat.new(params)
+        @chat = Chat.new(project_params)
         if @chat.save
           puts("Chat saved")
         end
     end
+  
+  private
+  def project_params
+    params.require(:chat).permit(:name, :users, :messages)
+  end
 end

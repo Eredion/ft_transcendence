4a272bd09d42887d1e1829579586c47d55e56f65
David R <egoz3ntrum@gmail.com>
2021-02-25 15:00:16 +0100

    wars fixed

--- setup/myapp/app/jobs/war_start_job.rb
+++ setup/myapp/app/jobs/war_start_job.rb
@@ -4,8 +4,13 @@ class WarStartJob < ApplicationJob
   def perform(war)
     war.status = "active"
     war.save
+    war.guilds.each do |guild|
+      guild.inwar = true
+      guild.save
+      end
+    end
     puts "WAR IS NOW ON"
     # Do something later
-    WarEndJob.set(wait_until: war.startdate + war.duration.minutes).perform_later(war)
+    WarEndJob.set(wait_until: Time.now + war.duration.minutes).perform_later(war)
   end
 end

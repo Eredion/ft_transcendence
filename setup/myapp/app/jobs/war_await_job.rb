f7b21d76b07c0e506167aa2009e22faf1fb28481
David R <egoz3ntrum@gmail.com>
2021-02-26 14:39:58 +0100

    current user mistery solved?

--- setup/myapp/app/jobs/war_await_job.rb
+++ setup/myapp/app/jobs/war_await_job.rb
@@ -3,16 +3,18 @@ class WarAwaitJob < ApplicationJob
 
   def perform(war)
     (0..war.duration).each do |n|
+      wartimeduration = 2.minutes
       wartime = war.startdate + war.wartimehour.hours + n.days
       #wartime = Time.now + n.days
       if (n == 0 && wartime < Time.now && wartime > Time.now - 1.hours)
-        WarTimeOnJob.perform_later(war)                           ## war was started inside wartime
+        WarTimeOnJob.perform_later(war)
+        WarTimeOffJob.set(wait_until: wartime + wartimeduration).perform_later(war)                           ## war was started inside wartime
       elsif (n == 0 && wartime < Time.now && wartime < Time.now - 1.hours)
         puts "no wartime today"                                   ## first day of war has no wartime
       else
         WarTimeOnJob.set(wait_until: wartime).perform_later(war)  ## normal wartime
+        WarTimeOffJob.set(wait_until: wartime + wartimeduration).perform_later(war)
       end
-      WarTimeOffJob.set(wait_until: wartime + 1.minutes).perform_later(war)
     end
     if (war.startdate == Date.today)
       WarStartJob.perform_later(war)

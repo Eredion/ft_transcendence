b9590395e36e472665b4c845bcc694af594657d5
Eredion <alvaro.segovia@hotmail.com>
2021-03-03 14:05:07 +0100

    Fixed error in ranking

--- setup/myapp/app/javascript/packs/views/rankingView.js
+++ setup/myapp/app/javascript/packs/views/rankingView.js
@@ -51,10 +51,13 @@ let chatView = Backbone.View.extend({
             let output3 = template3({'tournaments': self.tournCol.toJSON()});
             $('#tournament-ranking').html(output3);
             let tournament = self.tournCol.findWhere({'status':'active'})
-            let template4 = _.template($("#tournament-ranking-template").html());
-            usersOrdered.sort((a, b) => (b.tournament_victories - b.tournament_defeats) - (a.tournament_victories - a.tournament_defeats));
-            let output4 = template4({'users': usersOrdered, 'tournament': tournament.toJSON()});
-            $('#current-tournament-ranking').html(output4);
+            if (tournament != null)
+            {
+                let template4 = _.template($("#tournament-ranking-template").html());
+                usersOrdered.sort((a, b) => (b.tournament_victories - b.tournament_defeats) - (a.tournament_victories - a.tournament_defeats));
+                let output4 = template4({'users': usersOrdered, 'tournament': tournament.toJSON()});
+                $('#current-tournament-ranking').html(output4);
+            }
         });
     },
 });
